49cb342387e582fd701535d08a405edfdf306e61
Administrator <Administrator@Vincent-PC>
2016-10-09 21:06:23 +0800

    优化注释

--- src/main/java/com/job/common/utils/ApplicationContext.java
+++ src/main/java/com/job/common/utils/ApplicationContext.java
@@ -10,13 +10,22 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.ServletContextAware;
 
 /**
- * 将version版本号写入application中，给css,js引用时用
+ * 
+ * @Description 将version版本号写入application中，给css,js引用时用
+ * @author 王鑫 
+ * @date Oct 9, 2016 8:39:46 PM
  */
 @Component
 public class ApplicationContext implements ServletContextAware {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationContext.class);
 
+    /**
+     * 
+     * <p>初始化到Application作用域当中</p> 
+     * @param context 
+     * @see org.springframework.web.context.ServletContextAware#setServletContext(javax.servlet.ServletContext)
+     */
     @Override
     public void setServletContext(ServletContext context) {
         String datetime = DateUtil.dateToString(Calendar.getInstance().getTime(), DateUtil.fm_yyyyMMddHHmmssSSS);
